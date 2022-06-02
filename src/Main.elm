6583f8872344dd214536d92d8386788d73e26825
Зядик Дмитрий Сергеевич <dima@nvavia.ru>
2022-06-02 13:46:37 +0500

    add logo

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ import Element.Input as Input
 type Msg
     = NoOp
     | ButtonPress
+    | ButtonPress2
 
 
 type alias Model =
@@ -43,15 +44,50 @@ view model =
             column []
                 [ header
                 , text model.message
-                , viewPreview "./image.jpg"
+                , newFunction model
+                , footer
                 ]
         ]
     }
 
 
+newFunction model =
+    case model.showImage of
+        True ->
+            text "Нету"
+
+        False ->
+            viewPreview "./image.jpg"
+
+
+color =
+    { blue = rgb255 0x72 0x9F 0xCF
+    , darkCharcoal = rgb255 0x2E 0x34 0x36
+    , lightBlue = rgb255 0xC5 0xE8 0xF7
+    , lightGrey = rgb255 0xE0 0xE0 0xE0
+    , white = rgb255 0xFF 0xFF 0xFF
+    }
+
+
+logo =
+    el
+        [ width <| px 40
+        , height <| px 40
+        , Border.width 2
+        , Border.rounded 6
+        , Border.color color.blue
+        ]
+    <|
+        viewPreview "./image.jpg"
+
+
 header =
-    row []
-        [ Input.button
+    row [ width fill, padding 20, spacing 20 ]
+        [ logo
+        , el [ Background.color <| color.blue, alignRight ] <| text "Меню1"
+        , el [ alignRight ] <| text "Меню2"
+        , el [ alignRight ] <| text "Меню3"
+        , Input.button
             [ Border.width 1
             , Border.rounded 4
             , paddingXY 8 4
@@ -60,8 +96,19 @@ header =
         ]
 
 
+footer =
+    row []
+        [ Input.button
+            [ Border.width 1
+            , Border.rounded 4
+            , paddingXY 8 4
+            ]
+            { label = text "Ищё жми жиж", onPress = Just ButtonPress2 }
+        ]
+
+
 viewPreview url =
-    image []
+    image [ width <| fill, height <| fill ]
         { src = url
         , description = "An image"
         }
@@ -74,3 +121,6 @@ update msg model =
 
         ButtonPress ->
             ( { message = "Пока", showImage = True }, Cmd.none )
+
+        ButtonPress2 ->
+            ( { message = "Пока", showImage = False }, Cmd.none )
